f8ba05671cad2bc29a4fbd154f9e1d43a85a6911
SHIGERU KAMADO <olomolapraiseoluwadamilare@gmail.com>
2025-09-21 10:37:15 +0100

    Customize Radix-Ui thema

--- Issues-Tracker/issuses-tracker/src/app/layout.tsx
+++ Issues-Tracker/issuses-tracker/src/app/layout.tsx
@@ -1,18 +1,16 @@
 import "@radix-ui/themes/styles.css";
+import './theme.css'
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import {Theme} from "@radix-ui/themes"
 import Navbar from "./Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const inter = Inter({
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -29,7 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body
       
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${inter.variable} antialiased`}
       >
         <Theme>
         <Navbar/>
